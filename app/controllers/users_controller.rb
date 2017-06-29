543a1fb6e566b5a0e44e8538f9c06ebe22295258
TruongMinhThuc <minhthuc229@gmail.com>
2017-06-29 15:27:17 +0700

    Sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      flash[:success] = t "user_signup.success-signup"
+      redirect_to @user
+    else
+      flash.now[:warning] = t "user_signup.warning-signup"
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+    return if @user
+    render file: "public/404.html"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root "static_pages#home"
   get "static_pages/:page", to: "static_pages#show"
+  resources :users, only: [:new, :create, :show]
 end
